8e80f84091e77faa2c94723c2c3f2e2a92d721fc
Wei Sun <sun@Weis-MacBook-Pro.local>
2023-11-10 10:42:13 +0800

    created authentication page component alongside signin form

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,7 +1,9 @@
 import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
 import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
+import SignInForm from '../../components/sign-in-form/sign-in-form.component';
+import './authentication.styles.scss';
 
-const SignIn = () => {
+const Authentication = () => {
 
     const logGoogleUser = async () => {
         const { user } = await signInWithGooglePopup();
@@ -10,12 +12,11 @@ const SignIn = () => {
 
 
     return (
-        <div>
-            <h1>Sign in page.</h1>
-            <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+        <div className='authentication-container'>
+            <SignInForm />
             <SignUpForm />
         </div>
     );
 };
 
-export default SignIn;
+export default Authentication;
